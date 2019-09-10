b88c9804c1771182f3f7cf1808ce3173edccf130
valmrtt <valeriaimoretti@gmail.com>
2019-09-09 21:10:32 -0300

    Cambiando validacion js

--- EJERCICIOS-JS/validar-link.js
+++ EJERCICIOS-JS/validar-link.js
@@ -1,21 +1,58 @@
 let links = document.querySelectorAll("nav a")
 
-links.forEach( manipularLink )
+/*links.forEach( manipularLink )*/
 
-function manipularLink(link){
+links.forEach( function (link){                  //<----funcion anonima = no tiene nombre, se asignan al momento de necesitarla
+	           console.log(link)
+
+	           link.onclick = 	function ( e ){                                              
+	
+				e.preventDefault()                         
+
+
+				console.log ("El culpable de este suceso es:")
+				console.log(e.target)                    
+
+
+				let url = e.target.href
+
+				let rta = confirm(`Esta segurx que desea ir a ${e.target.innerText}?`)
+
+				if( rta ){
+				alert("Bye~bee")
+				console.log("Hay que ir ak: " + url)
+				window.location.href = url
+				}
+				}	
+				}	 )
+
+/*function manipularLink(link){
 	//Aca manipulamos el link
 	console.log(link)
 
 	link.onclick = validar
-}
+}*/
 
-function validar( e ){                                              // e de evento
+
+
+/*function validar( e ){                                              // e de evento
 	
 	e.preventDefault()                         //<-- Frena el comportamiento predeterminado del link
 
-	let rta = confirm("Esta seguro que desea salir de la pag?")
+
+	console.log ("El culpable de este suceso es:")
+	console.log(e.target)                     //<--es EL elemento a traves del cual se provoca el evento
+
+
+	let url = e.target.href
+
+	let rta = confirm(`Esta segurx que desea ir a ${e.target.innerText}?`)
 
 	if( rta ){
 		alert("Bye~bee")
-	}
-}
+		console.log("Hay que ir ak: " + url)
+		window.location.href = url
+	}*/
+
+
+
